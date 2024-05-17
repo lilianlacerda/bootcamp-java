ff7877f91bbac857aac7377c8aa6ae848b0ee634
lilianlacerda <lilianraiany@gmail.com>
2024-05-16 22:13:34 -0300

    atualizações

---
+++ debuggingJava/src/CalculadoraDeMedia.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class CalculadoraDeMedia {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        String[] alunos = {"Camila", "Lucas", "Bruna", "Pedro"};
+
+        double media = calculaMediaTurma(alunos, scan);
+
+        System.out.printf("Média da turma %.1f", media);
+    }
+
+    public static double calculaMediaTurma(String[] alunos, Scanner scanner){
+        double soma = 0;
+        for(String aluno : alunos){
+            System.out.printf("Nota do aluno %s: ", aluno);
+            double nota = scanner.nextDouble();
+            soma += nota;
+        }
+
+        return soma / alunos.length;
+    }
+}
---
+++ debuggingJava/src/br/com/dio/debugging/Main.java
@@ -0,0 +1,28 @@
+package br.com.dio.debugging;
+
+public class Main {
+    public static void main(String[] args){
+        System.out.println("Início do programa no método main.");
+        a();
+        System.out.println("Finalização do programa no método main.");
+    }
+
+    static void a(){
+        System.out.println("Início do método a.");
+        b();
+        System.out.println("Finalização do método a.");
+    }
+
+    static void b(){
+        System.out.println("Início do método b.");
+        for(int i = 0; i <= 4; i++) System.out.println(i);
+        c();
+        System.out.println("Finalização do método b");
+    }
+
+    static void c(){
+        System.out.println("Início do método c");
+        //Thread.dumpStok();
+        System.out.println("Finalização do método c");
+    }
+}
