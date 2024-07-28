3d4ed563a2ded47a19bda025ddc507485856ccd3
lilianlacerda <lilianraiany@gmail.com>
2024-07-28 14:52:05 -0300

    Add: novo projeto DIO

---
+++ projeto-api-rest-nuvem/projeto-bootcamp-java/src/test/java/me/dio/projeto_bootcamp_java/ProjetoBootcampJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package me.dio.projeto_bootcamp_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoBootcampJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
